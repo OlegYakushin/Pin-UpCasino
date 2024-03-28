869b9d94f6d48c29756c09db18a3c550385d7538
Oleg <yakushinoa@gmail.com>
2024-03-28 14:20:20 +0100

    Initial Commit

---
+++ Pin-UpCasino/Pin_UpCasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pin_UpCasinoApp.swift
+//  Pin-UpCasino
+//
+//  Created by Oleg Yakushin on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Pin_UpCasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
